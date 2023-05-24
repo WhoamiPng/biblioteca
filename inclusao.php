db23f39d37a03402bed59d111fce031e4434c9f9
Lucas Briahn <89462126+WhoamiPng@users.noreply.github.com>
2023-05-24 14:50:04 -0300

    Add files via upload

--- inclusao.php
+++ inclusao.php
@@ -1,25 +1,20 @@
 <?php
 // Conexão com o banco de dados (substitua pelos seus próprios detalhes de conexão)
-$host = "localhost";
-$dbname = "inventario";
-$username = "root";
-$password = "";
+include "conn.php";
 
 try {
     $conexao = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Captura as informações enviadas pelo formulário
-    $id = $_POST['id'];
     $nomeLivro = $_POST['nomeLivro'];
     $autor = $_POST['autor'];
     $genero = $_POST['genero'];
     $qtd = $_POST['qtd'];
 
     // Insere os dados na tabela "livros"
-    $query = "INSERT INTO livros (id, nomeLivro, autor, genero, qtd) VALUES (:id, :nomeLivro, :autor, :genero, :qtd)";
+    $query = "INSERT INTO livros (nomeLivro, autor, genero, qtd) VALUES ( :nomeLivro, :autor, :genero, :qtd)";
     $stmt = $conexao->prepare($query);
-    $stmt->bindParam(':id', $id);
     $stmt->bindParam(':nomeLivro', $nomeLivro);
     $stmt->bindParam(':autor', $autor);
     $stmt->bindParam(':genero', $genero);
