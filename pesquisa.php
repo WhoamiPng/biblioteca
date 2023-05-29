305a08674035611f9d9b31ab86c440bdea69bd2c
ViniciusOfEgg <110700454+ViniciusOfEgg@users.noreply.github.com>
2023-05-29 16:17:17 -0300

    Add files via upload

--- pesquisa.php
+++ pesquisa.php
@@ -2,7 +2,7 @@
 // Conexão com o banco de dados
 include 'conn.php';
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Falha na conexão com o banco de dados: " . $conn->connect_error);
 }
@@ -16,11 +16,13 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        echo "<li> ID do livro : " . $row["id"] . "</li>";
-        echo "<li> Nome do livro :" . $row["nomeLivro"] . "</li>";
-        echo "<li> Autor do livro :" . $row["autor"]  .  "</li>";
-        echo "<li> Genero do livro :" . $row["generos"] .  "</li>";
-        echo "<li> Quantidade de livros :" . $row["qtd"] .  "</li>";
+        echo "<tr>";
+        echo "<th>" . $row["id"] . "</th>";
+        echo "<th>" . $row["nomeLivro"] . "</th>";
+        echo "<th>" . $row["autor"]  .  "</th>";
+        echo "<th>" . $row["generos"] .  "</th>";
+        echo "<th>" . $row["qtd"] .  "</th>";
+        echo "</tr>";
 
     }
 } else {
