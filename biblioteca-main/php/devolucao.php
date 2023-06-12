06b85d656fb822e38f982ad9a902023a75df5e8d
ViniciusOfEgg <110700454+ViniciusOfEgg@users.noreply.github.com>
2023-06-12 19:36:13 -0300

    Add files via upload

---
+++ biblioteca-main/php/devolucao.php
@@ -0,0 +1,35 @@
+<?php
+     include "conn.php";
+
+     $conn = mysqli_connect($host,$username,$password,$dbname);
+     if(!$conn){
+         die("ERRO: " . mysqli_connect_error());
+     }
+ 
+     $id = $_POST["id"];
+     $status = 'devolvido';
+     $dataDevolucao = date("Y-m-d");
+
+     $sql = "SELECT * FROM emprestimo WHERE id='".$id."'";
+     $result = mysqli_query($conn,$sql);
+
+     if (mysqli_num_rows($result) > 0) {
+        if($row = $result->fetch_assoc()){
+            $sql = "INSERT INTO devolucao (nomeLivro, nomePessoa, dataDevolucao) VALUES ('".$row['nomeLivro']."','".$row['nomePessoa']."','".$dataDevolucao."')";
+            $result = mysqli_query($conn,$sql);
+            if($result){
+                $sql = "UPDATE emprestimo SET status='devolvido' WHERE id='".$id."'";
+                $result = mysqli_query($conn,$sql);
+                if($result){
+                    echo "DEVOLVIDO COM SUCESSO"; 
+                }else{
+                    echo "ERRO";
+                }
+            }else{
+                echo "ERRO";
+            }
+        }
+    } else {
+        echo "<li>Nenhum resultado encontrado.</li>";
+    }
+?>
